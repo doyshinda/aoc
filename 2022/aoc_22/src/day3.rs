61c052f1b13c8da0dad3bdcad89a5615d20b34ad
Abe Friesen <2319792+doyshinda@users.noreply.github.com>
2022-12-04 08:38:49 -0700

    2022 Day 3 improvements

--- 2022/aoc_22/src/day3.rs
+++ 2022/aoc_22/src/day3.rs
@@ -23,25 +23,17 @@ fn part_2() -> u64 {
     let data = util::read_input("3.input");
     let indexing = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     let lines = data.lines().collect::<Vec<&str>>();
-    let mut start = 0;
-    let mut sum = 0;
-    loop {
-        if start >= lines.len() {
-            break;
-        }
-        let e1_hash: HashSet<char> = lines[start].chars().collect();
-        let e2_hash: HashSet<char> = lines[start+1].chars().collect();
-        let e3_hash: HashSet<char> = lines[start+2].chars().collect();
+    lines
+        .chunks(3)
+        .map(|chunk| {
+            let e1_hash: HashSet<char> = chunk[0].chars().collect();
+            let e2_hash: HashSet<char> = chunk[1].chars().collect();
+            let e3_hash: HashSet<char> = chunk[2].chars().collect();
 
-        let intersection: HashSet<_> = &(&e1_hash & &e2_hash) & &e3_hash;
-        for i in intersection {
-            let p = indexing.find(i).unwrap() + 1;
-            sum += p as u64;
-        }
-        start += 3;
-    }
-
-    sum
+            let intersection: HashSet<_> = &(&e1_hash & &e2_hash) & &e3_hash;
+            intersection.iter().map(|i| (indexing.find(*i).unwrap() + 1) as u64).sum::<u64>()
+        })
+        .sum()
 }
 
 run!();
