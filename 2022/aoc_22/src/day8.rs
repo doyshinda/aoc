5fbf9151b0d1a45250d519590009701029263f15
Abe Friesen <2319792+doyshinda@users.noreply.github.com>
2022-12-09 19:48:52 -0700

    Update day8 to use Grid type

--- 2022/aoc_22/src/day8.rs
+++ 2022/aoc_22/src/day8.rs
@@ -1,16 +1,17 @@
 use crate::util;
+use griddy::Grid;
 
 fn part_1() -> u64 {
     let data = util::read_input("8.input");
-    let grid: Vec<Vec<u64>> = data.lines()
+    let grid = Grid::from_2d(data.lines()
         .map(|line| line.chars().map(|c| unum!(c.to_string())).collect::<Vec<u64>>())
-        .collect();
+        .collect::<Vec<Vec<u64>>>());
 
     let mut visible = 0;
 
-    for (row_idx, row) in grid.iter().enumerate() {
-        for col_idx in 0..row.len() {
-            if am_visible_row(row, col_idx) {
+    for row_idx in 0..grid.rows_len() {
+        for col_idx in 0..grid.cols_len() {
+            if am_visible_row(&grid, row_idx, col_idx) {
                 visible += 1;
                 continue;
             }
@@ -27,13 +28,13 @@ fn part_1() -> u64 {
 
 fn part_2() -> u64 {
     let data = util::read_input("8.input");
-    let grid: Vec<Vec<u64>> = data.lines()
+    let grid = Grid::from_2d(data.lines()
         .map(|line| line.chars().map(|c| unum!(c.to_string())).collect::<Vec<u64>>())
-        .collect();
+        .collect::<Vec<Vec<u64>>>());
 
     let mut score = 0;
-    for (row_idx, row) in grid.iter().enumerate() {
-        for col_idx in 0..row.len() {
+    for row_idx in 0..grid.rows_len() {
+        for col_idx in 0..grid.cols_len() {
             let this_score = get_score(&grid, row_idx, col_idx);
             if this_score > score {
                 score = this_score;
@@ -46,7 +47,7 @@ fn part_2() -> u64 {
 
 run!();
 
-fn get_score(grid: &Vec<Vec<u64>>, ridx: usize, cidx: usize) -> u64 {
+fn get_score(grid: &Grid<u64>, ridx: usize, cidx: usize) -> u64 {
     let mut score = score_up(grid, ridx, cidx);
     score *= score_down(grid, ridx, cidx);
     score *= score_left(grid, ridx, cidx);
@@ -54,61 +55,59 @@ fn get_score(grid: &Vec<Vec<u64>>, ridx: usize, cidx: usize) -> u64 {
     score
 }
 
-fn score_up(grid: &Vec<Vec<u64>>, ridx: usize, cidx: usize) -> u64 {
+fn score_up(grid: &Grid<u64>, ridx: usize, cidx: usize) -> u64 {
     let mut score = 0;
-    for row in grid[..ridx].iter().rev() {
+    for (r, c) in grid.col_up_coords(ridx, cidx).iter().rev() {
         score += 1;
-        if row[cidx] >= grid[ridx][cidx] {
+        if grid[*r][*c] >= grid[ridx][cidx] {
             break;
         }
     }
     score
 }
 
-fn score_down(grid: &Vec<Vec<u64>>, ridx: usize, cidx: usize) -> u64 {
+fn score_down(grid: &Grid<u64>, ridx: usize, cidx: usize) -> u64 {
     let mut score = 0;
-    for row in grid[ridx+1..].iter() {
+    for (r, c) in grid.col_down_coords(ridx, cidx).iter() {
         score += 1;
-        if row[cidx] >= grid[ridx][cidx] {
+        if grid[*r][*c] >= grid[ridx][cidx] {
             break;
         }
     }
     score
 }
 
-fn score_left(grid: &Vec<Vec<u64>>, ridx: usize, cidx: usize) -> u64 {
+fn score_left(grid: &Grid<u64>, ridx: usize, cidx: usize) -> u64 {
     let mut score = 0;
-    let row = &grid[ridx];
-    for col in row[..cidx].iter().rev() {
+    for (r, c) in grid.row_left_coords(ridx, cidx).iter().rev() {
         score += 1;
-        if *col >= grid[ridx][cidx] {
+        if grid[*r][*c] >= grid[ridx][cidx] {
             break;
         }
     }
     score
 }
 
-fn score_right(grid: &Vec<Vec<u64>>, ridx: usize, cidx: usize) -> u64 {
+fn score_right(grid: &Grid<u64>, ridx: usize, cidx: usize) -> u64 {
     let mut score = 0;
-    let row = &grid[ridx];
-    for col in row[cidx+1..].iter() {
+    for (r, c) in grid.row_right_coords(ridx, cidx).iter() {
         score += 1;
-        if *col >= grid[ridx][cidx] {
+        if grid[*r][*c] >= grid[ridx][cidx] {
             break;
         }
     }
     score
 }
 
-fn am_visible_col(grid: &Vec<Vec<u64>>, me_col_idx: usize, me_row_idx: usize) -> bool {
-    if me_row_idx == 0 || me_row_idx == grid.len() - 1 {
+fn am_visible_col(grid: &Grid<u64>, col_idx: usize, row_idx: usize) -> bool {
+    if row_idx == 0 || row_idx == grid.rows_len() - 1 {
         return true;
     }
-    let me = grid[me_row_idx][me_col_idx];
+    let me = grid[row_idx][col_idx];
     // Check up
     let mut am_visible = true;
-    for row in grid[..me_row_idx].iter() {
-        if row[me_col_idx] >= me {
+    for (r, c) in grid.col_up_coords(row_idx, col_idx) {
+        if grid[r][c] >= me {
             am_visible = false;
             break;
         }
@@ -120,8 +119,8 @@ fn am_visible_col(grid: &Vec<Vec<u64>>, me_col_idx: usize, me_row_idx: usize) ->
 
     // Check down
     am_visible = true;
-    for row in grid[me_row_idx+1..].iter() {
-        if row[me_col_idx] >= me {
+    for (r, c) in grid.col_down_coords(row_idx, col_idx) {
+        if grid[r][c] >= me {
             am_visible = false;
             break;
         }
@@ -130,13 +129,13 @@ fn am_visible_col(grid: &Vec<Vec<u64>>, me_col_idx: usize, me_row_idx: usize) ->
     return am_visible;
 }
 
-fn am_visible_row(row: &Vec<u64>, me_col_idx: usize) -> bool {
-    let me = row[me_col_idx];
+fn am_visible_row(grid: &Grid<u64>, row_idx: usize, col_idx: usize) -> bool {
+    let me = grid[row_idx][col_idx];
     let mut am_visible = true;
 
     // Check left
-    for col in row[..me_col_idx].iter() {
-        if *col >= me {
+    for (r, c) in grid.row_left_coords(row_idx, col_idx) {
+        if grid[r][c] >= me {
             am_visible = false;
             break;
         }
@@ -147,9 +146,10 @@ fn am_visible_row(row: &Vec<u64>, me_col_idx: usize) -> bool {
 
     // Check right
     am_visible = true;
-    for col in row[me_col_idx+1..].iter() {
-        if *col >= me {
+    for (r, c) in grid.row_right_coords(row_idx, col_idx) {
+        if grid[r][c] >= me {
             am_visible = false;
+            break;
         }
     }
 
@@ -161,13 +161,13 @@ mod tests {
     use super::*;
     #[test]
     fn test_am_visible_col() {
-        let grid = vec![
+        let grid = Grid::from_2d(vec![
             vec![3, 0, 3, 7, 3],
             vec![2, 5, 5, 1, 2],
             vec![6, 5, 3, 3, 2],
             vec![3, 3, 5, 4, 9],
             vec![3, 5, 3, 9, 0],
-        ];
+        ]);
         assert_eq!(false, am_visible_col(&grid, 0, 1));
         assert_eq!(true, am_visible_col(&grid, 1, 1));
         assert_eq!(true, am_visible_col(&grid, 2, 1));
@@ -179,23 +179,29 @@ mod tests {
 
     #[test]
     fn test_am_visible_row() {
-        let row = vec![2, 5, 5, 1, 2];
-        assert_eq!(true, am_visible_row(&row, 0));
-        assert_eq!(true, am_visible_row(&row, 1));
-        assert_eq!(true, am_visible_row(&row, 2));
-        assert_eq!(false, am_visible_row(&row, 3));
-        assert_eq!(true, am_visible_row(&row, 4));
-    }
-
-    #[test]
-    fn test_score() {
-        let grid = vec![
+        let grid = Grid::from_2d(vec![
             vec![3, 0, 3, 7, 3],
             vec![2, 5, 5, 1, 2],
             vec![6, 5, 3, 3, 2],
             vec![3, 3, 5, 4, 9],
             vec![3, 5, 3, 9, 0],
-        ];
+        ]);
+        assert_eq!(true, am_visible_row(&grid, 1, 0));
+        assert_eq!(true, am_visible_row(&grid, 1, 1));
+        assert_eq!(true, am_visible_row(&grid, 1, 2));
+        assert_eq!(false, am_visible_row(&grid, 1, 3));
+        assert_eq!(true, am_visible_row(&grid, 1, 4));
+    }
+
+    #[test]
+    fn test_score() {
+        let grid = Grid::from_2d(vec![
+            vec![3, 0, 3, 7, 3],
+            vec![2, 5, 5, 1, 2],
+            vec![6, 5, 3, 3, 2],
+            vec![3, 3, 5, 4, 9],
+            vec![3, 5, 3, 9, 0],
+        ]);
         assert_eq!(1, score_up(&grid, 1, 2));
         assert_eq!(2, score_down(&grid, 1, 2));
         assert_eq!(1, score_left(&grid, 1, 2));
