fe35a73178b3fad2ada928124235d9837a8518f7
Abe Friesen <2319792+doyshinda@users.noreply.github.com>
2024-12-05 20:24:06 -0800

    Performance improvements day4 2024

--- 2024/aoc_2024/src/day4.rs
+++ 2024/aoc_2024/src/day4.rs
@@ -1,33 +1,15 @@
 use crate::util;
-use griddy::Grid;
+use griddy::{Direction, OptGrid};
 
-fn check_neighbors(grid: &Grid<char>, coords: &[(usize, usize)]) -> u64 {
-    if grid[coords[0].0][coords[0].1] == 'M'
-        && grid[coords[1].0][coords[1].1] == 'A'
-        && grid[coords[2].0][coords[2].1] == 'S'
-    {
-        return 1;
-    }
-    0
-}
-
-fn check_grid(grid: &Grid<char>, x: usize, y: usize) -> u64 {
+fn check_grid(grid: &OptGrid, x: isize, y: isize) -> u64 {
     let mut ans = 0;
-    let funcs = vec![
-        Grid::row_left_coords,
-        Grid::row_right_coords,
-        Grid::col_up_coords,
-        Grid::col_down_coords,
-        Grid::diag_right_up,
-        Grid::diag_right_down,
-        Grid::diag_left_up,
-        Grid::diag_left_down,
-    ];
+    for (rs, cs) in Direction::ALL {
+        let one = grid.get(x + (rs * 1), y + (cs * 1)).unwrap_or(&b' ');
+        let two = grid.get(x + (rs * 2), y + (cs * 2)).unwrap_or(&b' ');
+        let three = grid.get(x + (rs * 3), y + (cs * 3)).unwrap_or(&b' ');
 
-    for f in funcs {
-        let rlc = f(&grid, x, y);
-        if rlc.len() >= 3 {
-            ans += check_neighbors(grid, &rlc);
+        if *one == b'M' && *two == b'A' && *three == b'S' {
+            ans += 1;
         }
     }
 
@@ -37,14 +19,12 @@ fn check_grid(grid: &Grid<char>, x: usize, y: usize) -> u64 {
 fn part_1() -> u64 {
     let data = util::read_input("4.input");
     let mut ans = 0;
-    let grid = data.split("\n").map(|l| l.chars().collect()).collect();
-    let grid = Grid::from_2d_unchecked(grid);
+    let grid = OptGrid::new(&data, '\n');
 
-    for x in 0..grid.rows_len() {
-        for y in 0..grid.cols_len() {
-            if grid[x][y] == 'X' {
-                ans += check_grid(&grid, x, y);
-            }
+    for (x, y, c) in grid.clone() {
+        match c {
+            b'X' => { ans += check_grid(&grid, x, y) },
+            _ => (),
         }
     }
 
@@ -54,32 +34,23 @@ fn part_1() -> u64 {
 fn part_2() -> u64 {
     let data = util::read_input("4.input");
     let mut ans = 0;
-    let grid = data.split("\n").map(|l| l.chars().collect()).collect();
-    let grid = Grid::from_2d_unchecked(grid);
+    let grid = OptGrid::new(&data, '\n');
+    let valid: u16 = ((b'M' as u16) * 2) + ((b'S' as u16) * 2);
 
-    for x in 0..grid.rows_len() {
-        for y in 0..grid.cols_len() {
-            hm!(map, char, usize);
-            if grid[x][y] == 'A' {
-                let diag = grid.diag_neighbors(x, y);
-                for (a, b) in &diag {
-                    hm_inc!(map, grid[*a][*b], 1);
+    for (x, y, c) in grid.clone() {
+        if c == b'A' {
+            let count = Direction::DIAGONAL.iter().fold(0u16, |acc, (rs, cs)| {
+                match grid.get(x + rs, y + cs) {
+                    Some(v) => acc + (*v as u16),
+                    None => acc,
                 }
-                let mut mcount = 0;
-                let mut scount = 0;
-                if let Some(count) = map.get(&'M') {
-                    mcount = *count;
-                }
-                if let Some(count) = map.get(&'S') {
-                    scount = *count;
-                }
+            });
 
-                if scount == 2 && mcount == 2 {
-                    let a = grid[diag[0].0][diag[0].1];
-                    let b = grid[diag[3].0][diag[3].1];
-                    if a != b {
-                        ans += 1;
-                    }
+            if count == valid {
+                let a = grid.get(x + Direction::UPRIGHT.0, y + Direction::UPRIGHT.1).unwrap();
+                let b = grid.get(x + Direction::DOWNLEFT.0, y + Direction::DOWNLEFT.1).unwrap();
+                if a != b {
+                    ans += 1;
                 }
             }
         }
@@ -89,20 +60,3 @@ fn part_2() -> u64 {
 }
 
 run!();
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test() {
-        let data = util::read_input("4_test.input");
-        let mut ans = 0;
-        let grid = data.split("\n").map(|l| l.chars().collect()).collect();
-        let grid = Grid::from_2d_unchecked(grid);
-
-        let coords = [(1, 5), (2, 6), (3, 7)];
-        ans = check_neighbors(&grid, &coords);
-        assert_eq!(1, ans);
-    }
-}
